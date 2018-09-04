4eeb363485441afd22033b293b180227fbf82935
Tomas Kaukinen <tomaskaukinen@gmail.com>
2018-09-04 10:44:45 -0400

    VBA Code (Hockey Data Collector.xlsm)

--- HockeyPlayerStatsScraping.vb
+++ HockeyPlayerStatsScraping.vb
@@ -1,11 +1,13 @@
 Option Explicit
 
-Const year1 As Integer = 2017 'Change this to the most recent season
-'Refers to year1-(year1 + 1) Season
+Const year1 As Integer = 2017 'Change this to the most recently COMPLETED season
+'Refers to year1/(year1 + 1) Season
 
 '*************************************************************************************************
 '***IMPORTANT***After this macro runs, the columns need to be rearranged so that they match previous seasons, _
 'and the titles of each column need to match previous season titles
+'*** Previous data was web scraped from a different source, so the way the tables are constructed are _
+'different (Yahoo Sports vs. Sportsnet)
 '*************************************************************************************************
 
 Sub NHLStats() '****Web scrapes latest season of hockey player data
@@ -14,19 +16,19 @@ Sub NHLStats() '****Web scrapes latest season of hockey player data
 'https://stackoverflow.com/questions/34703533/how-to-scrape-data-from-the-following-table-format-vba
 'https://www.ozgrid.com/forum/forum/other-software-applications/excel-and-web-browsers-help/145319-select-an-item-from-a-dropdown-list-on-webpage
 Dim IE As Object 'Internet explorer
-Dim r As Long 'Row
-Dim c As Long 'Column
+Dim TableRow As Long 'Row
+Dim TableCol As Long 'Column
 Dim t As Long 'Child element
-Dim rSheet As Long 'Keeps track of what is the current row in the worksheet _
+Dim SheetRow As Long 'Keeps track of what is the current row in the worksheet _
 'for placing hockey player data
-Dim rStart As Long 'start place for scraping the data from the elements in elementstable
-Dim bReady As Boolean 'Pagination either available or not
-Dim elementsTable As Object 'Hockey player data
-Dim elementsPageNavRigth As Object 'Pagination
-Dim elemPageNavRigth As Object 'Pagination
-Dim elementsTableDiv As Object 'Hockey player data
+Dim RowStart As Long 'start place for scraping the data from the elements in HockeyStatsTable
+Dim PagEnd As Boolean 'Pagination either available or not
+Dim HockeyStatsTable As Object 'Hockey player data
+Dim PagButton As Object 'Pagination
+Dim NavPagRight As Object 'Pagination
+Dim HockeyStatsDiv As Object 'Hockey player data
 Dim DOMevent As Object 'Object to allow event to execute from drop down menu
-Dim sel As Object
+Dim SeasonSelect As Object
 Dim SheetToFind As String 'Worksheet for most recent hockey season
 Dim sheet As Variant 'Contains every worksheet name
 
@@ -57,17 +59,17 @@ With IE
     Do While IE.readyState = 4: DoEvents: Loop
     Do Until IE.readyState = 4: DoEvents: Loop
 
-    rSheet = 0 'Starting value for row location on worksheet
+    SheetRow = 0 'Starting value for row location on worksheet
     
     'Fires the onchange event
     'Selects a specific option from a dropdown menu
     Set DOMevent = IE.document.createEvent("HTMLEvents")
     DOMevent.initEvent "change", True, False
     
-    Set sel = IE.document.getElementById("season-dropdown") 'Dropdown menu name by ID
-    sel.Focus
-    sel.selectedIndex = 1  '2nd option from drop down menu (0 would be 1st option)
-    sel.dispatchEvent DOMevent 'Execute the event
+    Set SeasonSelect = IE.document.getElementById("season-dropdown") 'Dropdown menu name by ID
+    SeasonSelect.Focus
+    SeasonSelect.selectedIndex = 1  '2nd option from drop down menu (0 would be 1st option)
+    SeasonSelect.dispatchEvent DOMevent 'Execute the event
     
     'Short time delay to allow the event to execute and webpage to load
     Application.Wait (Now + TimeValue("0:00:03"))
@@ -75,52 +77,52 @@ With IE
     Do 'Will loop until pagination limit reached
         
         'Find the element containing the Table of data for hockey players
-        Do While elementsTableDiv Is Nothing
-             Set elementsTableDiv = IE.document.getElementsByClassName("dataTables_scrollBody")
+        Do While HockeyStatsDiv Is Nothing
+             Set HockeyStatsDiv = IE.document.getElementsByClassName("dataTables_scrollBody")
              DoEvents
         Loop
         
-        Do While elementsTableDiv(0) Is Nothing 'Ensure that the elements exist within variable
+        Do While HockeyStatsDiv(0) Is Nothing 'Ensure that the elements exist within variable
              DoEvents
         Loop
         
         'Find where pagination occurs and with the element name
-        Set elementsPageNavRigth = IE.document.getElementsByClassName("paginate_button next")
+        Set PagButton = IE.document.getElementsByClassName("paginate_button next")
         'Child element for pagination navigating to the right
-        Set elemPageNavRigth = elementsPageNavRigth(0)
+        Set NavPagRight = PagButton(0)
         
         'Name of element for when pagination is no longer available
-        'If this is the name of the child element then bReady becomes true, signalling _
+        'If this is the name of the child element then PagEnd becomes true, signalling _
         'that there is no more data to be collected
-        If elemPageNavRigth.className = "paginate_button next disabled" Then bReady = True
+        If NavPagRight.className = "paginate_button next disabled" Then PagEnd = True
         
         'Decides what data will be collected
-        'When rstart is = -1 then it will collect the row of data containing the headings of the columns
-        If rSheet = 0 Then rStart = -1 Else rStart = 0
+        'When RowStart is = -1 then it will collect the row of data containing the headings of the columns
+        If SheetRow = 0 Then RowStart = -1 Else RowStart = 0
         
-        'Collects elements from table of hockey player data from the table within elementstablediv
-        Set elementsTable = elementsTableDiv(0).getElementsByTagName("TABLE")
+        'Collects elements from table of hockey player data from the table within HockeyStatsDiv
+        Set HockeyStatsTable = HockeyStatsDiv(0).getElementsByTagName("TABLE")
    
         'This portion finds each piece of data from the table and places it in a cell within _
         'the worksheet
-        For r = rStart + 1 To (elementsTable(0).rows.Length - 1) 'Defines beginning position and _
+        For TableRow = RowStart + 1 To (HockeyStatsTable(0).rows.Length - 1) 'Defines beginning position and _
         'ending position regarding the rows within table
             'Defines beginning position and ending position regarding the columns within table
-            For c = 0 To (elementsTable(0).rows(r).Cells.Length - 1)
+            For TableCol = 0 To (HockeyStatsTable(0).rows(TableRow).Cells.Length - 1)
                 'Placement of data within worksheet
-                ThisWorkbook.ActiveSheet.Cells(r + rSheet + 1, c + 1) = elementsTable(t).rows(r).Cells(c).innerText
-            Next c
-        Next r
+                ThisWorkbook.ActiveSheet.Cells(TableRow + SheetRow + 1, TableCol + 1) = HockeyStatsTable(t).rows(TableRow).Cells(TableCol).innerText
+            Next TableCol
+        Next TableRow
     
-        rSheet = rSheet + r 'Saves row location within worksheet
+        SheetRow = SheetRow + TableRow 'Saves row location within worksheet
     
-        If Not elemPageNavRigth Is Nothing Then elemPageNavRigth.Click 'Execute pagination
+        If Not NavPagRight Is Nothing Then NavPagRight.Click 'Execute pagination
 
-        Set elementsTableDiv = Nothing 'Erase most recent hockey player data from variable
+        Set HockeyStatsDiv = Nothing 'Erase most recent hockey player data from variable
 
         Application.Wait (Now + TimeValue("0:00:01"))
         
-    Loop Until bReady Or elemPageNavRigth Is Nothing 'If pagination not avaiable then exit loop
+    Loop Until PagEnd Or NavPagRight Is Nothing 'If pagination not avaiable then exit loop
 
 End With
 
